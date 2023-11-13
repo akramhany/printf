95e14fb7f218d46b6889ee83f3a613fbc17eb123
akramhany <akramhany65@gmail.com>
2023-11-13 09:33:45 -0800

    implement the basics of printf and handle c, s, % formats

---
+++ printf.c
@@ -0,0 +1,77 @@
+#include <stdarg.h>
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * _printf - output a string to the std output.
+ * @format: The string that will be outputed to the stdout.
+ * @...: The variabels that would be outputed in the string.
+ *
+ * Return: the number of characters printed (excluding the null byte used to end the output string)
+ */
+
+int _printf(const char *format, ...)
+{
+	va_list ap;
+	int formatLength = 0, literals = 0;
+	int i = 0, bufferSize = 0, totalPrintedChars = 0;
+	char *buffer;
+
+	while (format[formatLength] != '\0')
+	{
+		if (format[formatLength] == '%' && format[formatLength + 1] != '%')
+			literals++;
+
+		formatLength++;
+	}
+
+	va_start(ap, literals);
+	
+	buffer = malloc(formatLength);
+
+	totalPrintedChars = formatLength - 1;
+
+	while (i < formatLength)
+	{
+		if (format[i] == '%')
+		{
+			Write(1, buffer, bufferSize);
+			bufferSize = 0;
+			i++;
+
+			if (format[i] == 'c')
+			{
+				char c = va_arg(ap, char);
+				Write(1, c, 1);
+				totalPrintedChars++;
+			}
+			else if (format[i] = 's')
+			{
+				int k = 0;
+				char *s = va_arg(ap, char*);
+
+				while (s[k] != '\0')
+				{
+					k++;
+				}
+				Write(1, s, k);
+				totalPrintedChars += (k - 1);
+			}
+			else if (format[i] = '%')
+			{
+				Write(1, '%', 1);
+				totalPrintedChars++;
+			}
+
+		}
+		else
+		{
+			buffer[bufferSize] = format[i];
+			bufferSize++;
+		}
+
+		i++;
+	}
+
+return (totalPrintedChars);
+}
